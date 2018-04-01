ce92f47f747239c8b633b9dc6d60861306ef6de7
Domok Gergely <emanreyalp@users.noreply.github.com>
2018-04-01 14:25:45 +0200

    Set db connection with .env

--- db.js
+++ db.js
@@ -1,4 +1,16 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://'+process.env.DB_HOST+'/hotstock');
+
+mongoose.connect(
+	process.env.DB_HOST, {
+		user: process.env.DB_USER,
+		pass: process.env.DB_PASS,
+		dbName: process.env.DB_NAME
+	},
+	function(err) {
+		if (err){
+			console.log(err);
+	}
+});
 
 module.exports = mongoose;
+
