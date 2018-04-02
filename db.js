7b24249aa4261db18665cfbb29593475d94c80cf
Domok Gergely <emanreyalp@users.noreply.github.com>
2018-04-02 11:23:08 +0200

    Use connection string for db connection

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.connect(
-	process.env.DB_HOST, {
-		user: process.env.DB_USER,
-		pass: process.env.DB_PASS,
-		dbName: process.env.DB_NAME
-	},
+	process.env.DB_CONNECTION_STRING,
 	function(err) {
 		if (err){
 			console.log(err);
