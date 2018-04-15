24549513328e3e48fc5fad93baf67a971aca08ab
veresspeter <veress.peter@simonyi.bme.hu>
2018-04-15 20:17:20 +0200

    Auth Logging

--- middlewares/general/authAdmin.js
+++ middlewares/general/authAdmin.js
@@ -2,7 +2,11 @@ module.exports = function (objectrepository) {
 
     return function (req, res, next) {
 
-        if ( !req.isAuthenticated() || !req.session.passport || req.session.passport.user.permission < 2 ) {
+        if (!req.isAuthenticated() || !req.session.passport || req.session.passport.user.permission < 2) {
+
+            var userID = res.tpl.func.userID(req) ;
+
+            res.tpl.func.logger.warn("Unauthorized user - Access denied to Admin function ( IP: " + res.tpl.func.reqIP(req) + " )");
             var err = new Error('Unauthorized');
             err.status = 401;
             return next(err);
--- middlewares/general/authUser.js
+++ middlewares/general/authUser.js
@@ -3,6 +3,10 @@ module.exports = function (objectrepository) {
     return function (req, res, next) {
 
         if ( !req.isAuthenticated() ) {
+
+            var userID = res.tpl.func.userID(req) ;
+
+            res.tpl.func.logger.warn("Unauthorized user - Access denied to User function ( IP: " + res.tpl.func.reqIP(req) + " )");
             var err = new Error('Unauthorized');
             err.status = 401;
             return next(err);
