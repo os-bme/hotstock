a32d36dd0088b7e54598296f7dc1be7b00cc3f36
veresspeter <veress.peter@simonyi.bme.hu>
2018-04-16 17:49:55 +0200

    Redirect MWs and Rendering MWs logging

--- middlewares/general/reverseAuth.js
+++ middlewares/general/reverseAuth.js
@@ -2,6 +2,7 @@ module.exports = function (objectrepository) {
 
     return function (req, res, next) {
         if (req.isAuthenticated()) {
+            res.tpl.func.logger.info("Redirect to \'/\' ");
             res.redirect('/');
         } else {
             return next();
