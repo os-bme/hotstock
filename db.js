58e378a6367e5f680b70fd1691077c9ff3a65365
veresspeter <veress.peter@simonyi.bme.hu>
2018-05-27 13:00:44 +0200

    Connect with mongoose to MongoDb update with useMongoClient

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 var mongoose = require('mongoose');
 
 mongoose.connect(
-	process.env.DB_CONNECTION_STRING,
-	function(err) {
-		if (err){
-			console.log(err);
-	}
-});
+    process.env.DB_CONNECTION_STRING,
+    {useMongoClient: true},
+    function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
 
 module.exports = mongoose;
 
